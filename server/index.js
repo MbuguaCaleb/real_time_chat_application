93f5cdd0ae0dafeb12459a24009c5e8ea18a7d41
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-09-17 19:23:47 +0300

    heroku-deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
@@ -78,7 +79,7 @@ io.on('connection', (socket) => {
 
 //calling the router as a middleware
 app.use(router);
-
+app.use(cors());
 server.listen(PORT, () => {
   console.log(`Server has started on PORT ${PORT}`);
 });
